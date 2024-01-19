8459771e99efbd6d043ebae8b1c8597bb20d6a8a
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-18 21:29:47 -0800

    Add code and styling for comment component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.scss';
 import Header from './components/Header/Header';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import VideoComponent from './components/VideoComponent/VideoComponent';
 import data from './data/video-details.json';
+import Comment from './components/Comment/Comment';
 
 function App() {
   return (
     <>
       <Header/>
       <VideoComponent key={data[0].id} video={data[0]} />
+      <Comment/>
     </>
   )
 }
