f5594cc78603fa5be57720fe3deb3a91a46152a1
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-25 22:55:55 -0800

    Add styling for desktop screen size

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -6,26 +6,28 @@ export default function UploadVideo() {
     return (
         <section className='upload-video'>
             <h1 className='upload-video__page-header'>Upload Video</h1>
-            <div>
-                <h2 className='upload-video__subheader'>VIDEO THUMBNAIL</h2>
-                <img src={videoThumbnail} alt='upload video preview' className='upload-video__thumbnail'/>
-            </div>
-            <section className='upload-video__container'>
-            <form className='upload-video__form-section'>
-                <div className='upload-video__title-container'>
-                    <label htmlFor='videoTitle' className='upload-video__form-title'>TITLE YOUR VIDEO</label>
-                    <input type='text' name='videoTitle' id='videoTitle' placeholder='Add a title to your video'></input>
+            <section className='upload-video__main-section'>
+                <div className='upload-video__video-thumbnail'>
+                    <h2 className='upload-video__subheader'>VIDEO THUMBNAIL</h2>
+                    <img src={videoThumbnail} alt='upload video preview' className='upload-video__thumbnail'/>
                 </div>
-                <div className='upload-video__description-container'>
-                    <label htmlFor='videoDescription' className='upload-video__form-title '>ADD A VIDEO DESCRIPTION</label>
-                    <input type='text' name='videoDescription' id='videoDescription' placeholder='Add a description to your video'></input>
+                <div className='upload-video__container'>
+                    <form className='upload-video__form-section'>
+                        <div className='upload-video__title-container'>
+                            <label htmlFor='videoTitle' className='upload-video__form-title'>TITLE YOUR VIDEO</label>
+                            <input type='text' name='videoTitle' id='videoTitle' placeholder='Add a title to your video'></input>
+                        </div>
+                        <div className='upload-video__description-container'>
+                            <label htmlFor='videoDescription' className='upload-video__form-title '>ADD A VIDEO DESCRIPTION</label>
+                            <input type='text' name='videoDescription' id='videoDescription' placeholder='Add a description to your video'></input>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </section>
             <div className='upload-video__publish-btn'>
                 <Button buttonText="PUBLISH" variant="button__upload-video"/>
                 <h3 className='upload-video__cancel'>CANCEL</h3>
             </div>
-            </section>
         </section>
     )
 }
