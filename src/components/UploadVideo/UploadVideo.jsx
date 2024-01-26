72130d324baf88850ac0bc64695806a33a580fe6
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-01-25 22:18:59 -0800

    Add style for tablet screen

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -12,17 +12,19 @@ export default function UploadVideo() {
             </div>
             <section className='upload-video__container'>
             <form className='upload-video__form-section'>
-                <div className='upload-video__input-container'>
+                <div className='upload-video__title-container'>
                     <label htmlFor='videoTitle' className='upload-video__form-title'>TITLE YOUR VIDEO</label>
                     <input type='text' name='videoTitle' id='videoTitle' placeholder='Add a title to your video'></input>
                 </div>
-                <div className='upload-video__input-container'>
-                    <label htmlFor='videoDescription' className='upload-video__form-title'>ADD A VIDEO DESCRIPTION</label>
+                <div className='upload-video__description-container'>
+                    <label htmlFor='videoDescription' className='upload-video__form-title '>ADD A VIDEO DESCRIPTION</label>
                     <input type='text' name='videoDescription' id='videoDescription' placeholder='Add a description to your video'></input>
                 </div>
             </form>
-            <Button buttonText="PUBLISH" variant="button__upload-video"/>
-            <h3 className='upload-video__cancel'>CANCEL</h3>
+            <div className='upload-video__publish-btn'>
+                <Button buttonText="PUBLISH" variant="button__upload-video"/>
+                <h3 className='upload-video__cancel'>CANCEL</h3>
+            </div>
             </section>
         </section>
     )
